a03f65f2d8f41ff40075bce492a68e7e2e61a0d4
dorrajmachai <dwimble22@protonmail.com>
2023-03-25 08:21:17 -0600

    getting nvm set up with fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias config "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
-	fish_add_path "~/.cargo/bin"
+	fish_add_path "$HOME/.cargo/bin"
+	fish_add_path "$HOME/.cargo/bin/neovide"
 end

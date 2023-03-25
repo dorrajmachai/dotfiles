57b61e9aa137591ce82ea7b2cb8abe2f682dc11b
dorrajmachai <dwimble22@protonmail.com>
2023-03-25 07:49:29 -0600

    adding stuff to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias config "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
+	fish_add_path "~/.cargo/bin"
 end

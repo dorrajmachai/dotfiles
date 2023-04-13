2e02a70ad3ccea74d11b58e0f194bcee20665f42
dorrajmachai <dwimble22@protonmail.com>
2023-04-13 02:13:25 -0600

    haven't committed changes in a while

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,8 +7,12 @@ if status is-interactive
 	fish_add_path "$HOME/.config/nvim/lua-language-lerver/bin/lua-language-server"
 	fish_add_path "$HOME/bin"
 	fish_add_path "/opt/gradle/gradle-8.0.2/bin"
+	fish_add_path "$HOME/.rbenv/bin"
+	fish_add_path "$HOME/.rbenv/plugins/ruby-build/bin/"
 
+	eval "$(rbenv init -)"
 	alias exercism "/home/emaget_/bin/exercism-3.1.0-linux-x86_64/exercism"
 	alias nv "nvim"
     alias v "vim"
+	alias emacs "emacs --maximized"
 end

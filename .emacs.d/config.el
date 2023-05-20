b8525d869e2cb1921dc5b754aebf55fba144e8e3
dorrajmachai <dwimble22@protonmail.com>
2023-05-20 15:19:37 -0600

    big commit

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -1,5 +1,12 @@
 (setq inhibit-startup-message t) ;; This line disables the startup message
-(tool-bar-mode -1) ;; this line should disable the toolbar
+(tool-bar-mode -1) ;; this line will disable the toolbar
 (menu-bar-mode -1) ;; this line will disable the menu bar
+(toggle-scroll-bar -1) ;; this line will disable the scroll bar
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'catppuccin t)
+
+(setq catppuccin-flavor 'latte)
+(catppuccin-reload)
 
 
